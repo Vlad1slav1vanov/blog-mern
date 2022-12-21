095d5647257308a5b052b1237a1ac32d441eb464
Vladislav <typlomi@gmail.com>
2022-12-21 19:52:23 +0600

    create User model

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect('mongodb+srv://userblog:userblog1@cluster0.84kwvd0.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('DB OK'))
+  .catch((err) => console.log('DB Error: ' + err))
 
 const app = express();
 
@@ -12,10 +18,12 @@ app.get('/', (req, res) => {
 app.post('/auth/login', (req, res) => {
   console.log(req.body)
 
-  const token = jwt.sign({
-    email: req.body.email,
-    fullName: 'Владислав Иванов'
-  }, 'secret123')
+  if (req.body.email === 'test@test.ru') {
+    const token = jwt.sign({
+      email: req.body.email,
+      fullName: 'Владислав Иванов'
+    }, 'secret123')
+  }
 
   res.json({
     success: true,
