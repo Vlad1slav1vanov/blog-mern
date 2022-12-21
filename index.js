030a1ca0657ae2a84a7fdcd2dd63411b8afe9d95
Vladislav <typlomi@gmail.com>
2022-12-22 01:30:21 +0600

    create CRUD for posts

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import {registerValidation, loginValidation} from './validations.js';
+import {registerValidation, loginValidation, postCreateValidation} from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 
 mongoose
@@ -11,12 +12,17 @@ mongoose
   .catch((err) => console.log('DB Error: ' + err))
 
 const app = express();
-
 app.use(express.json())
 
 app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
-app.get('/auth/me', checkAuth, UserController.getMe)
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 app.listen(9000, (err) => {
   if (err) {
