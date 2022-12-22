cdd16333644eca9a483a06bc386c066645ad2ad1
Vladislav <typlomi@gmail.com>
2022-12-22 16:26:04 +0600

    init frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
+import Container from "@mui/material/Container";
+
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+
 function App() {
   return (
     <>
+      <Header />
+      <Container maxWidth="lg">
+        <Home />
+        {/*<FullPost />*/}
+        {/*<AddPost />*/}
+        {/*<Login />*/}
+        {/*<Registration />*/}
+      </Container>
     </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.scss";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <App />
+  <React.StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
