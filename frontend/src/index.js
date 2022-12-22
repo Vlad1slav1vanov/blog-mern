d8ae1cd106b67181909c523e433bd6321bcd131b
Vladislav <typlomi@gmail.com>
2022-12-22 17:08:37 +0600

    delete node modules

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
