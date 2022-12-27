eda6bd313b5507bf727a68e5532e1d8731337039
Vladislav <typlomi@gmail.com>
2022-12-27 21:34:23 +0600

    Create comment schema

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -9,7 +9,7 @@ export const getLastTags = async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({
-      message: 'Не удалось получить статьи'
+      message: 'Не удалось получить тэги'
     });  
   }
 }
@@ -74,7 +74,7 @@ export const getOne = async (req, res) => {
           message: 'Статья не найдена',
         })
       };
-
+ 
       res.json(doc);
     }
     ).populate('user');
---
+++ backend/models/Comment.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const CommentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('Comment', CommentSchema);
